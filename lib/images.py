ccfdb7f380f94520989e6b7a1f59a6d4d96007c9
Evaldo Junior <evaldojuniorbento@gmail.com>
2014-05-07 15:26:57 -0300

    Updating license information (LGPL now)

--- lib/images.py
+++ lib/images.py
@@ -8,7 +8,7 @@
 # Copyright (C) 2013 Dafiti TI Team <ti@dafiti.com.br>
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the Lesser GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
@@ -17,7 +17,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the Lesser GNU General Public License
 # along with this program. If not, see http://www.gnu.org/licenses/.
 
 from wand.image import Image
